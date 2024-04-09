dcdcb7540f439f1a6a4a293577a737895016c7e3
Javi <javiercamarerolopez@gmail.com>
2024-04-09 14:48:09 +0200

    fix

--- pelis.sql
+++ pelis.sql
@@ -6,8 +6,9 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE 'usuarios'(
     'id' INTEGER NOT NULL UNIQUE,
     'nombre' TEXT NOT NULL  UNIQUE,
-    'peliculafav' TEXT , 
-    PRIMARY KEY ('id' AUTOINCREMENT)
+    'peliculafav' INTEGER , 
+    PRIMARY KEY (id AUTOINCREMENT)
+    FOREIGN KEY (favorita) REFERENCES peliculas (id),
 ) STRICT;
 
 CREATE TABLE 'peliculas'(
@@ -16,21 +17,23 @@ CREATE TABLE 'peliculas'(
     'genero' TEXT  ,
     'año' INTEGER ,
     'director' TEXT,
-    'autor' INTEGER NOT NULL,
-    FOREIGN KEY ('autor') REFERENCES 'usuarios' ('id'),
-    PRIMARY KEY ('id' AUTOINCREMENT)
+    PRIMARY KEY (id AUTOINCREMENT)
 ) STRICT;
 
 
-INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario0', 'Jaws');
-INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario1', 'Spider-Man');
-INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario2', 'Van Helsing');
-INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario3', 'Dracula Untold');
-INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario4', 'Sonic the Hedgehog 2');
+INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario0', 0);
+INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario1', 1);
+INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario2', 2);
+INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario3', 3);
+INSERT INTO usuarios (nombre, peliculafav) VALUES ('Usuario4', 4);
 
 
-INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('Jaws', 'Terror', 1975, 'Steven Spielberg', 0);
-INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('Spider-Man', 'Ciencia Ficción', 2002, 'Sam Raimi', 1);
-INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('Van Helsing', 'Fantasía Terrorifica', 2004, 'Stephen Sommers', 2);
-INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('Dracula Untold', 'Fantasía Terrorifica', 2015, 'Gary Shore', 3);
-INSERT INTO peliculas (titulo, genero, año, director, autor) VALUES ('Sonic the Hedgehog 2', 'Aventura', 2022, 'Jeff Fowler', 4);
+INSERT INTO peliculas (titulo, genero, año, director) VALUES ('Jaws', 'Terror', 1975, 'Steven Spielberg');
+INSERT INTO peliculas (titulo, genero, año, director) VALUES ('Spider-Man', 'Ciencia Ficción', 2002, 'Sam Raimi');
+INSERT INTO peliculas (titulo, genero, año, director) VALUES ('Van Helsing', 'Fantasía Terrorifica', 2004, 'Stephen Sommers');
+INSERT INTO peliculas (titulo, genero, año, director) VALUES ('Dracula Untold', 'Fantasía Terrorifica', 2015, 'Gary Shore');
+INSERT INTO peliculas (titulo, genero, año, director) VALUES ('Sonic the Hedgehog 2', 'Aventura', 2022, 'Jeff Fowler');
+
+--SELECT usuarios.nombre, peliculas.nombre AS pelicula_favorita
+--FROM usuarios
+--JOIN usuarios.peliculafav = peliculas.id;
