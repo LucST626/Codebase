4c20789230cd2fe39cb86963f55b15f1be2a155c
Luc626 <lucasst626@gmail.com>
2024-04-11 15:57:44 +0200

    pain-t

--- socketio/index.js
+++ socketio/index.js
@@ -10,13 +10,26 @@ const io = new Server(server);
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
+   
+app.get('/paint', (req, res) => {
+  res.sendFile(join(__dirname, 'paint.html'));
+});
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
     });
+    socket.on('paint', (datos) => {
+         io.emit('paint', datos);
+         console.log(datos);
+       });
+
+    socket.on('paint', (datos) => {
+    const item = document.createElement('ellipse');
+    item.textContent = datos;
+    messages.appendChild(item);
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+  console.log('server running at http://localhost:3000')}
+);
